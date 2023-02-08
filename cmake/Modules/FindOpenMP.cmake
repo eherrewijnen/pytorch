e4eede3a72936dead0031455f4b0020a46801ecd
cyy <cyyever@outlook.com>
2023-02-08 23:29:54 +0800

    Detect MKL omp only when mkl blas is specified. Fix the problem of infinite inclusion and fix detection of mkl on ubuntu.

--- cmake/Modules/FindOpenMP.cmake
+++ cmake/Modules/FindOpenMP.cmake
@@ -227,8 +227,9 @@ function(_OPENMP_GET_FLAGS LANG FLAG_MODE OPENMP_FLAG_VAR OPENMP_LIB_NAMES_VAR)
     #   http://openmp.llvm.org/
     #
     # So here, before we test each flag combination, we first try directly
-    # linking against any `libomp` MKL has found (if any). This allows us to
-    # do sensible things in tricky (yet common) conditions like:
+    # linking against any `libomp` MKL has linked to (if any and when MKL is
+    # specified). This allows us to do sensible things in tricky (yet common)
+    # conditions like:
     #   - using `clang` (so no native GNU OpenMP), and
     #   - having `brew` `libomp` installed at `/usr/local/`, and
     #   - having `conda` `mkl` installed at `$HOME/conda/`, with includes a copy
@@ -239,7 +240,7 @@ function(_OPENMP_GET_FLAGS LANG FLAG_MODE OPENMP_FLAG_VAR OPENMP_LIB_NAMES_VAR)
     # Crucially, we only do so for non-GNU compilers. For GNU ones,
     # `FindMKL.cmake` calls `FindOpenMP.cmake` when trying to find `gomp` and
     # thus will cause infinite recursion if this is not taken care of. Moreover,
-    # for them, since the compiler provices the OpenMP library, it is most
+    # for them, since the compiler provides the OpenMP library, it is most
     # likely that only one viable gomp library can be found in search path by
     # `FindOpenMP.cmake`, so the chance of having conflicts is slow.
     #
@@ -247,8 +248,10 @@ function(_OPENMP_GET_FLAGS LANG FLAG_MODE OPENMP_FLAG_VAR OPENMP_LIB_NAMES_VAR)
     #         - for non-GNU, FindOpenMP.cmake replies on FindMKL.cmake to finish first, but
     #         - for GNU,     FindMKL.cmake replies on FindOpenMP.cmake to finish first.
 
-    if(NOT "${CMAKE_${LANG}_COMPILER_ID}" STREQUAL "GNU")
+    if(NOT "${CMAKE_${LANG}_COMPILER_ID}" STREQUAL "GNU" AND BLAS STREQUAL "MKL" AND NOT IN_FIND_OMP)
+      set(IN_FIND_OMP ON CACHE BOOL "" FORCE)
       find_package(MKL QUIET)
+      unset(IN_FIND_OMP CACHE)
       if(MKL_FOUND AND MKL_OPENMP_LIBRARY)
         # If we already link OpenMP via MKL, use that. Otherwise at run-time
         # OpenMP will complain about being initialized twice (OMP: Error #15),
